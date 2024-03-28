c13d8eb9e997c0e922e130720c4ae58ace193aa8
gitaunthama <gitaunthama@gmail.com>
2024-03-28 14:58:02 +0300

    complete work

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+// const dodger = document.getElementById("dodger");
+// // dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+// dodger.style.bottom = "100px";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+function moveDodgerLeft() {
+    document.getElementById("dodger").style.left = 
+    (parseInt(document.getElementById("dodger").style.left) 
+    || 0) - 1 + "px";
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+function moveDodgerRight() {
+    var dodger = document.getElementById("dodger");
+    dodger.style.left = (parseInt(dodger.style.left) || 0) + 1 + "px";
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
